810d8b9d8605911f5efb6bf70854855087c4fa59
Neo <97616742+neolynx001@users.noreply.github.com>
2022-07-30 13:57:30 +0530

    Added HelloWorld.ino file

---
+++ HelloWorld_LCD.ino
@@ -0,0 +1,36 @@
+
+//SDA=A4
+//SCL=A5
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(0,9);
+  lcd.print("Tinkerers lab");
+
+}
+//lcd.setCursor(0, 0);
+//              lcd.print(" UNREGISTERED              "); 
+//              delay(500);
+//              lcd.setCursor(0, 1);
+//              lcd.print(".........");
+//                             
+//                  for (int positionCounter = 0; positionCounter < 43; positionCounter++) {
+//    // scroll one position left:
+//    lcd.scrollDisplayLeft();
+//         // wait a bit:
+//    delay(150);
+
+
+void loop()
+{
+}
